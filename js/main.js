e08d89e19bae59db4766bc010eb47c8ded82c757
jaimin pandya <jaiminmoslake7020@gmail.com>
2020-03-17 19:41:00 -0700

    Animation events setup

--- js/main.js
+++ js/main.js
@@ -10,54 +10,49 @@ class App{
 
     constructor(){
         this._stopInterval = 0 ;
-       // this.screenAnimationSetup();
+        this.screenAnimationSetup();
     }
 
     screenAnimationSetup(){
         let screenItemActive = document.getElementById('screen-content').querySelector('.screen-item.active');
         let selfObject = this;
         selfObject.makePhoneAppearEffectStart( screenItemActive );
-
         let interval = setInterval(function () {
-
             if( selfObject.stopInterval ){
                 clearInterval( interval );
             }
-
-            let screenItemActive = document.getElementById('screen-content').querySelector('.screen-item.active');
-            let nextElementSibling = null ;
-            if( screenItemActive.length === 0 ){
-                nextElementSibling = document.getElementById('screen-content').querySelector('.screen-item');
-            }else{
-                nextElementSibling = screenItemActive.nextElementSibling;
-            }
-
-            if( nextElementSibling === null ){
-                nextElementSibling = document.getElementById('screen-content').querySelector('.screen-item');
-            }
-
-            screenItemActive.classList.remove('active');
-            nextElementSibling.classList.add('active');
-
-            selfObject.makePhoneAppearEffect( screenItemActive , nextElementSibling );
-
+            selfObject.animateScreen( null );
         },5000);
+        this.setupAnimationEvents();
+    }
 
+    setupAnimationEvents(){
+        Array.from( document.querySelectorAll('.screen-item:not(.active)>.article-title') , function ( el ) {
+            el.addEventListener('click',function () {
+                console.log(this.parentElement)
+                app.animateScreen( this.parentElement );
+            })
+        });
+    }
+
+    animateScreen( nextElementSibling ){
+        let screenItemActive = document.getElementById('screen-content').querySelector('.screen-item.active');
+        if( nextElementSibling == null ){
+            nextElementSibling = this.getNextElementSibling( screenItemActive );
+        }
+        screenItemActive.classList.remove('active');
+        nextElementSibling.classList.add('active');
+        this.makePhoneAppearEffect( screenItemActive , nextElementSibling );
     }
 
     makePhoneAppearEffect( screenItemActive , nextElementSibling ){
         setTimeout(function () {
-
             screenItemActive.querySelector('.screen-box').classList.remove('active');
             nextElementSibling.querySelector('.screen-box').classList.add('active');
-
             setTimeout(function () {
-
                 screenItemActive.querySelector('.screen-regular').classList.remove('active');
                 nextElementSibling.querySelector('.screen-regular').classList.add('active');
-
             },100);
-
         },600);
     }
 
@@ -70,14 +65,34 @@ class App{
         },600);
     }
 
-
+    getNextElementSibling( screenItemActive ){
+        let nextElementSibling = null ;
+        if( screenItemActive.length === 0 ){
+            nextElementSibling = document.getElementById('screen-content').querySelector('.screen-item');
+        }else{
+            nextElementSibling = screenItemActive.nextElementSibling;
+        }
+        if( nextElementSibling === null ){
+            nextElementSibling = document.getElementById('screen-content').querySelector('.screen-item');
+        }
+        return nextElementSibling;
+    }
 
 }
 
 let app = null;
 window.addEventListener('load',function () {
       app = new App();
+
 });
+
+
+
+
+
+
+
+
 
 
 
