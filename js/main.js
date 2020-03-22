8b9623c6aeaa9613d70d8ec4fd34245556be1467
jaimin pandya <jaiminmoslake7020@gmail.com>
2020-03-22 12:57:52 -0700

    .gitignore

--- js/main.js
+++ js/main.js
@@ -1,4 +1,114 @@
-class App{
+let app = null;
+window.addEventListener('load',function () {
+    app = new App();
+    app.scroll.init();
+});
+
+
+class base {
+
+    constructor() {
+
+    }
+
+    init(){
+
+    }
+
+    activate( id ){
+        let sections = document.querySelectorAll('.main-section');
+        Array.from( sections , function ( section ) {
+            section.classList.remove('active');
+        });
+        document.getElementById(id).classList.add('active');
+    }
+
+    deActivate( id ){
+        document.getElementById(id).classList.remove('active');
+    }
+
+}
+
+class App extends base{
+
+    get topSection() {
+        return this._topSection;
+    }
+
+    get needs() {
+        return this._needs;
+    }
+
+    get screens() {
+        return this._screens;
+    }
+
+
+    get scroll() {
+        return this._scroll;
+    }
+
+    constructor() {
+        super();
+        this._topSection = new topSection();
+        this._needs = new needs();
+        this._screens = new screens();
+        this._scroll = new scroll();
+
+        this.init();
+    }
+
+    init() {
+        let selfObject = this;
+        window.addEventListener('scroll',function(){
+            selfObject.scroll.init();
+        });
+    }
+
+}
+
+class topSection extends base{
+    constructor() {
+        super();
+        this.init();
+    }
+
+    init() {
+        this._section = document.getElementById('top-content');
+        let articleTitle = this._section.querySelector('.article-title');
+        let appCafes = document.querySelector('.app-cafes').querySelectorAll('.cafe');
+        setTimeout(function () {
+
+            Array.from( appCafes , function ( appCafe ) {
+                appCafe.classList.add('active');
+            } );
+
+            document.querySelector('.app-welcome-screen').querySelector('.screen-item').classList.add('active');
+            articleTitle.classList.add('active');
+
+        },500);
+    }
+
+    activate( id ){
+        super.activate( id )
+        this.init();
+    }
+
+    deActivate(id) {
+        super.deActivate(id);
+        this._section = document.getElementById('top-content');
+        let articleTitle = this._section.querySelector('.article-title');
+        let appCafes = document.querySelector('.app-cafes').querySelectorAll('.cafe');
+        Array.from( appCafes , function ( appCafe ) {
+            appCafe.classList.remove('active');
+        } );
+        document.querySelector('.app-welcome-screen').querySelector('.screen-item').classList.remove('active');
+        articleTitle.classList.remove('active');
+    }
+
+}
+
+class screens extends base {
 
     get stopInterval() {
         return this._stopInterval;
@@ -9,14 +119,44 @@ class App{
     }
 
     constructor(){
+        super();
+        this.makeTitleInSpanList();
+        this.init();
+    }
+
+    activate( id ){
+        super.activate( id );
+        let screenItemActive = document.getElementById('screen-content').querySelector('.screen-item.active');
+        this.makePhoneAppearEffectStart( screenItemActive );
+    }
+
+    init() {
+        super.init();
+
+        let selfObject = this;
         this._stopInterval = 0 ;
-        this.screenAnimationSetup();
+        new Promise(function (resolve, reject) {
+            selfObject.makeTitleInSpanList();
+            resolve();
+        }).then(function () {
+            selfObject.screenAnimationSetup();
+        }).catch(function ( error ) {
+            console.log( error );
+        });
+    }
+
+    makeTitleInSpanList(){
+        let articleTitles = document.getElementById('screen-content').querySelectorAll('.article-title');
+        Array.from( articleTitles , function ( articleTitle ) {
+            let articleTitleHtml = articleTitle.textContent ;
+            articleTitle.innerHTML = '<span>' + articleTitleHtml.split("").join('</span><span>') + '</span>';
+        });
     }
 
     screenAnimationSetup(){
         let screenItemActive = document.getElementById('screen-content').querySelector('.screen-item.active');
         let selfObject = this;
-        selfObject.makePhoneAppearEffectStart( screenItemActive );
+        //selfObject.makePhoneAppearEffectStart( screenItemActive );
         let interval = setInterval(function () {
             if( selfObject.stopInterval ){
                 clearInterval( interval );
@@ -27,10 +167,11 @@ class App{
     }
 
     setupAnimationEvents(){
+        let selfObject = this;
         Array.from( document.querySelectorAll('.screen-item:not(.active)>.article-title') , function ( el ) {
             el.addEventListener('click',function () {
                 console.log(this.parentElement);
-                app.animateScreen( this.parentElement );
+                selfObject.animateScreen( this.parentElement );
             })
         });
     }
@@ -80,10 +221,112 @@ class App{
 
 }
 
-let app = null;
-window.addEventListener('load',function () {
-      app = new App();
-});
+class needs extends base{
+
+    constructor() {
+        super();
+    }
+
+    init() {
+        super.init();
+
+        document.querySelector('.question').classList.add('active');
+        document.querySelector('.answers.answer-1').classList.add('active');
+        document.querySelector('.answers.answer-2').classList.add('active');
+        document.querySelector('.answers.answer-3').classList.add('active');
+
+    }
+
+    activate(id) {
+        super.activate(id);
+        this.init()
+    }
+
+    deActivate(id) {
+        super.deActivate(id);
+
+        document.querySelector('.question').classList.remove('active');
+        document.querySelector('.answers.answer-1').classList.remove('active');
+        document.querySelector('.answers.answer-2').classList.remove('active');
+        document.querySelector('.answers.answer-3').classList.remove('active');
+
+    }
+
+
+}
+
+class scroll extends base{
+    constructor() {
+        super();
+    }
+
+    init() {
+        let selfObject = this;
+        this._pageStatus = null ;
+
+        let pageTop = window.visualViewport.pageTop ;
+        let usePageTop = pageTop+( document.querySelector('.main-section').offsetHeight/2 );
+
+        Array.from( document.querySelectorAll('.main-section') , function ( section ) {
+            let sectionTopPoint = section.offsetTop ;
+            let sectionBottomPoint = section.offsetTop+section.offsetHeight ;
+
+            if(  section.id === "top-content" ){
+                console.log(  sectionTopPoint );
+                console.log( usePageTop );
+                console.log(  sectionBottomPoint );
+
+                console.log(  sectionTopPoint <= usePageTop );
+                console.log( ( sectionTopPoint === 0 && usePageTop < 400 )  );
+                console.log( sectionBottomPoint > usePageTop   );
+                console.log( selfObject._pageStatus !== section.id  );
+                console.log( selfObject._pageStatus  );
+                console.log(  section.id  );
+            }
+
+            if( ( sectionTopPoint <= usePageTop || ( sectionTopPoint === 0 && usePageTop < 400 ) ) && sectionBottomPoint > usePageTop && selfObject._pageStatus !== section.id ){
+                console.log('at '+section.id);
+                selfObject._pageStatus = section.id;
+
+                switch ( selfObject._pageStatus ) {
+                    case "top-content" :
+                        app.topSection.activate( selfObject._pageStatus );
+                        app.needs.deActivate( "needs-content" );
+                        break;
+                    case "needs-content" :
+                        app.needs.activate( selfObject._pageStatus );
+                        app.topSection.deActivate( "top-content" );
+                        app.topSection.deActivate( "features-content" );
+                        break;
+                    case "features-content" :
+                        app.activate( selfObject._pageStatus );
+                        app.screens.deActivate( "screen-content" );
+                        app.needs.deActivate( "needs-content" );
+                        break;
+                    case "screen-content" :
+                        app.screens.activate( selfObject._pageStatus );
+                        app.topSection.deActivate( "testimonials-content" );
+                        app.topSection.deActivate( "features-content" );
+                        break;
+                    case "testimonials-content" :
+                        app.activate( selfObject._pageStatus );
+                        app.topSection.deActivate( "team-content" );
+                        app.screens.deActivate( "screen-content" );
+                        break;
+                    case "team-content" :
+                        app.activate( selfObject._pageStatus );
+                        app.topSection.deActivate( "testimonials-content" );
+                        break;
+                }
+
+            }
+        });
+
+    }
+}
+
+
+
 
 
 
